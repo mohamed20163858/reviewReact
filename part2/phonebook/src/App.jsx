f6a428575c45d272c1612670ab5be0cf731a6009
Mohamed Saleh <mohamed20163858@gmail.com>
2026-08-29 00:02:42 +0300

    Finish exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const filtered = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterValue.toLowerCase()),
+  );
   const handleSubmit = (e) => {
     e.preventDefault();
     let found = persons.find((person) => person.name === newName);
     if (found !== undefined) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
     }
     document.getElementById("name").value = "";
     setNewName("");
+    document.getElementById("number").value = "";
+    setNewNumber("");
   };
 
   return (
@@ -20,7 +32,25 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input id="name" onChange={(e) => setNewName(e.target.value)} />
+          filter shown with:{" "}
+          <input onChange={(e) => setFilterValue(e.target.value)} />
+        </div>
+        <h4>add a new</h4>
+        <div>
+          name:{" "}
+          <input
+            id="name"
+            onChange={(e) => setNewName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            id="number"
+            onChange={(e) => setNewNumber(e.target.value)}
+            required
+          />
         </div>
         {/* <div>{newName}</div> */}
         <div>
@@ -30,9 +60,18 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
+
+      {filtered.length === 0
+        ? persons.map((person) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))
+        : filtered.map((person) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))}
     </div>
   );
 };
