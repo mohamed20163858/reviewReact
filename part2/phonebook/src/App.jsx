eb01457eb5115d942c723dca3bc9a2b7af1f82c0
Mohamed Saleh <mohamed20163858@gmail.com>
2026-08-28 23:11:39 +0300

    Finish exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        {/* <div>{newName}</div> */}
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+      ;
+    </div>
+  );
+};
+
+export default App;
