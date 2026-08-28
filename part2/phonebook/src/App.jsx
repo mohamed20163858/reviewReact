98ece85c731ab6e2289c6ebd9db0965dcd284514
Mohamed Saleh <mohamed20163858@gmail.com>
2026-08-28 23:32:11 +0300

    Finish exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,7 +5,13 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPersons(persons.concat({ name: newName }));
+    let found = persons.find((person) => person.name === newName);
+    if (found !== undefined) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName }));
+    }
+    document.getElementById("name").value = "";
     setNewName("");
   };
 
@@ -14,7 +20,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={(e) => setNewName(e.target.value)} />
+          name: <input id="name" onChange={(e) => setNewName(e.target.value)} />
         </div>
         {/* <div>{newName}</div> */}
         <div>
@@ -27,7 +33,6 @@ const App = () => {
       {persons.map((person) => (
         <p key={person.name}>{person.name}</p>
       ))}
-      ;
     </div>
   );
 };
