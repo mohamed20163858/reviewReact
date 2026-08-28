a8af75ca9c5600ad00a50501e9e4f7184945da2e
Mohamed Saleh <mohamed20163858@gmail.com>
2026-08-28 19:13:26 +0300

    Finish exercises from 2.1 till 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -19,31 +19,13 @@ const Content = (props) => {
 };
 const Total = (props) => {
   return (
-    <p>
-      Number of exercises{" "}
-      {props.parts.reduce((sum, part) => sum + part.exercises, 0)}
+    <p style={{ fontWeight: "bold" }}>
+      total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+      exercises
     </p>
   );
 };
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
@@ -51,6 +33,31 @@ const App = () => {
       <Total parts={course.parts} />
     </div>
   );
+};
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
 };
 
 export default App;
