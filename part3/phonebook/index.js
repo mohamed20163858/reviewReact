2915adb7bd7bf1b9f524e7fa1feb6248321abb74
Mohamed Saleh <mohamed20163858@gmail.com>
2026-09-23 20:31:46 +0300

    Finish exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,6 +27,12 @@ const port = 3001;
 app.get("/api/persons", (req, res) => {
   res.json(phonebookList);
 });
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(
+    `<p>Phonebook has info for ${phonebookList.length} people</p><p>${date}</p>`,
+  );
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
